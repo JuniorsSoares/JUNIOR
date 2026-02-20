24f22b9fc15165472215da058b7a16606d6dd380
Junior Soares <juniorolivergol@gmail.com>
2026-02-20 13:27:47 -0400

    chore: configurar deploy Vercel e gitignore

--- setup-git.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set GIT="C:\Program Files\Git\cmd\git.exe"
-%GIT% init
-%GIT% config user.email "juniorolivergol@gmail.com"
-%GIT% config user.name "Junior Soares"
-%GIT% add .
-%GIT% commit -m "feat: sistema de gincanas IASD Planalto - inicial"
-echo.
-echo === GIT INICIALIZADO COM SUCESSO! ===
